81b5f31ef1a9f66cf1289ebbffd8d25b1ef2216b
Rachel Wise <rachel.wise@locationlabs.com>
2017-03-16 05:36:51 -0700

    added templates and a better structured filesystem. included some logic for pulling and displaying public sets from a specific user to the /sets page

---
+++ app/run.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+import requests,time
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+	user = 'wuzzaface'
+	return render_template('index.html',title='Home',user=user)	
+
+@app.route('/sets')
+def sets():
+	setsrequest = requests.get('https://api.quizlet.com/2.0/users/wuzza_face/sets/?client_id=pyjFb2bnBN')
+	setsrequestjson = setsrequest.json()
+	titledateurl=([(item["title"],time.strftime('%m-%d-%Y', time.localtime(item["created_date"])),item["url"]) for item in setsrequestjson])
+	return render_template('sets.html',details=titledateurl)
+
+if __name__ == '__main__':
+    app.run(debug=True)
