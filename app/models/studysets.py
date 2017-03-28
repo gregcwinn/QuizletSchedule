9b87ec72ddbd0c2b5f8fb361e6566722f2adc464
WatserFace <wuzzaface@gmail.com>
2017-03-28 05:02:31 -0700

    Update studysets.py
    Updated comments

--- app/models/studysets.py
+++ app/models/studysets.py
@@ -3,33 +3,34 @@ import requests,pprint,datetime,time,json,os,itertools
 class StudySets:
 
   def __init__(self,setsrequestjson):
-    self.setsrequestjson = setsrequestjson
+    self.setsrequestjson = setsrequestjson  #current information for the public decks of the user from quizlet's api
 
-  #dates each deck was created converted from epoch time to a date object
-  #the day of the week as a numerical value (1-7:Mon-Sun) that corresponds with each date object for decks
+  #a list of days that the decks were created
+  #the day of the week is represented as a numerical value (1-7:Mon-Sun)
   def epochtodate(self):
     days = ([datetime.date.isoweekday(datetime.date.fromtimestamp(item["created_date"])) for item in self.setsrequestjson])
     return days
 
-  #ids of all of the decks that were imported
+  #a list of id's for all of the decks that were imported
   def listofids(self):
     ids = ([item["id"]for item in self.setsrequestjson])	
     return ids
 
-  #if history storage is empty, store all id's with numerical day of the week, and a laststudied value of "first study"
+  #if the history storage is empty, store the needed data from setsrequestjson
+  #needed data includes the id number, day of the week the deck was created, and an initial last studied value of "first study"
   def ishistoryempty(self,ids,days):
     if os.stat("testdata.json").st_size == 0:
       initialhistorydata = [{'id': idnum, 'laststudied': freq, 'dayofweek': day} for idnum, freq, day in zip(ids,itertools.repeat('firststudy'),days)]
       with open('testdata.json', 'w') as f:
         json.dump(initialhistorydata,f)
 
-  #open history storage file and load the json into history1
+  #retrieve the currently stored set data from history storage
   def gethistory(self):
     with open('testdata.json', "r") as f:
       historystorage = json.load(f)
     return historystorage
 
-  #check if any new sets have been added since the last history store. Add any new sets to the history store with a laststudied value of "first study"
+  #check if any new sets have been added since the last update to history storage. Add any new sets to the history storage with a last studied value of "first study"
   def addnewsets(self,historystorage):
     historyids = ([item["id"]for item in historystorage])	
     for set in self.setsrequestjson:
@@ -37,7 +38,8 @@ class StudySets:
         historystorage.append({'id': set["id"],'laststudied': 'firststudy','dayofweek': datetime.date.isoweekday(datetime.date.fromtimestamp(set["created_date"]))}) 
 
 
-  #Create an updated list of days for study based on history storage data
+  #determine the week day that each deck should study based on the history storage
+  #counting from the week day that the deck was created, firststudy advances by 1 day, secondstudy by 3, and weeklystudy is the same day of the week as the initial deck creation.
   def nextstudydays(self,historystorage):
     newstudydays = historystorage
     for studyday in newstudydays:
@@ -61,12 +63,12 @@ class StudySets:
         studyday["dayofweek"] = studyday["dayofweek"]
     return newstudydays
 
-  #calgulate what the current day of the week(Sunday,Monday,Tuesday...)
+  #calculates current day of the week and returns a corresponding numeric value
   def currentweekday(self):
     currentweekday = datetime.date.isoweekday((datetime.datetime.utcnow()))
     return currentweekday
 
-  #calculate the sets that should be studied based on the current day of the week
+  #calculates the sets that should be studied today based on the current day of the week
   def todaystudysets(self,newstudydays,currentweekday):
     for item in newstudydays:
       if item["dayofweek"] == currentweekday:
@@ -74,7 +76,7 @@ class StudySets:
     return todaystudysets
 
 
-  #update histore store for next lookup on sets that are currently being studied
+  #updates the history storage so that the last studied value is accurately reflected
   def updatehistory(self,historystorage,currentweekday):
     for item in historystorage:
        if item["dayofweek"] == currentweekday:
@@ -85,6 +87,7 @@ class StudySets:
     with open('testdata.json', "w") as f:
       json.dump(historystorage,f)
 
+  #returns the sets that need to be studied today as a list of dictionaries that include a title, url, and date of creation
   def getcurrentsets(self):
     self.ishistoryempty(self.listofids(),self.epochtodate())
     self.addnewsets(self.gethistory())
